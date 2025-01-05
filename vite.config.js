5aaebb71e43529308506b25ef6a92cc59eef8d95
risalat345 <ashrafuddinbbhuiyanrisalat>
2025-01-05 11:24:38 +0600

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default {
+// https://vite.dev/config/
+export default defineConfig({
   plugins: [react()],
   base:"/HealthHive-Frontend"
-};
+})
